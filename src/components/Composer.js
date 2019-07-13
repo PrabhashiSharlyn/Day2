30e5d11e59cfb22844e9951d940986585708cf6f
prabhashi.p <prabhashi.p@informaticsint.com>
2019-07-13 10:13:39 +0530

    Add component strcture

---
+++ src/components/Composer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Composer = () =>(
+    <div className="composer">
+        <div><textarea></textarea></div>
+        <button>Send</button>
+      </div>
+);
+
+export default Composer;
---
+++ src/components/Post.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Post = () => (
+    <div className="post">
+        <p>sdsdfsdfsd fsd fsd f sd fsd fsd f sd</p>
+
+        <p className="timestamp">12th July 2018</p>
+        <button className="action-btn edit-btn">Edit</button>
+        <button className="action-btn delete-btn">Delete</button>
+    </div>
+
+);
+
+export default Post;
